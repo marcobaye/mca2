ef951c74528505a8725ddef1915e2dbc580c4217
Marco <marco.baye@gmail.com>
2017-07-29 01:43:29 +0200

    added new, quasi-empty, game description file called game.py

---
+++ game.py
@@ -0,0 +1,36 @@
+#!/bin/cat
+# this is no python source, the file is only
+# called *.py to have syntax coloring!
+
+# see "example.py" for help about the command syntax in this file.
+
+# symbolic constants:
+define FALSE	0
+define TRUE	1
+
+# variables and default values:
+var score	0
+
+# items:
+# (args are size, start location, skript name, game name, game description)
+item small start palawaum "Palawaum" "ein normaler Palawaum aus sieben Banuzen"
+
+# "asm" passes the remainder of the line to the assembler backend unchanged:
+# these two are used for colored text output
+asm HINZ	= petscii_YELLOW	# puts "HINZ = petscii_YELLOW" into output file
+asm KUNZ	= petscii_LRED
+# ONLY USE THIS FOR SYMBOL DEFINITIONS, NOT FOR ACTUAL MACHINE CODE!
+
+# usages:
+#using ITEM1 ITEM2
+
+# procedures:
+#defproc name
+
+# locations:
+loc start
+	"Das Spiel start hier, weil dieser Ort 'start' heisst."
+	s2 anderso
+
+loc anderso
+	"This is another location."
